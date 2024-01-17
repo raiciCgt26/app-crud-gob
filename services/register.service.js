ee412ad957f484880367df757117d5fa0d56ee22
raiciCgt26 <sofgirl2601@gmail.com>
2024-01-17 18:11:32 -0400

    modf 5 6 7

--- services/register.service.js
+++
@@ -1,7 +0,0 @@
-import { Model } from "./model.js";
-
-export class RegisterService extends Model {
-  async registerUser(data) {
-    return this.execQuery("../php/register.php", data);
-  }
-}
--- services/user.service.js
+++
@@ -1,9 +0,0 @@
-// export class UserService extends Model {
-//     constructor() {
-//         super();
-//     }
-
-//     async getUsers() {
-//         const usuarios = await this.execQuery('SELECT * FROM usuarios');
-//     }
-// }
